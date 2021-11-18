c282fecba130f538f8ed14e7ba778d7fe1dee3e0
Alifudin Niko Priambodo <m.rozaki09903@gmail.com>
2021-11-18 08:31:14 +0700

    9. Menggunakan Package NPM

--- 8-NPM/app.js
+++ 8-NPM/app.js
@@ -1,1 +1,19 @@
 console.log('Hello');
+
+const validator = require('validator');
+const chalk = require('chalk');
+
+console.log(validator.isEmail('alif@gmail.com'));
+console.log(validator.isMobilePhone('085882218939', 'id-ID'));
+console.log(validator.isNumeric('085882218939'));
+
+
+console.log(chalk.blue('Hello world'));
+console.log(chalk.bgBlue('Hello world'));
+console.log(chalk.black.bgBlue('Hello world'));
+console.log(chalk.italic.black('Hello world'));
+
+const pesan = chalk`coba {bgBlue.black coba }`;
+console.log(chalk.bgRed.black(pesan));
+
+
